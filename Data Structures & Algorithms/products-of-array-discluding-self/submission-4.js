778be14b530de857b953c14a3ee576fe5b4715f0
Harsh Banjare <100964895+harshbanjare@users.noreply.github.com>
2026-04-20 18:30:52 +0530

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let results = []
+
+        let prefix = 1;
+        for(let i = 0; i < nums.length; i++){
+             results[i] = prefix;
+             prefix *= nums[i]
+        }
+
+
+        let postfix = 1;
+        for(let i = nums.length - 1; i >= 0; i--){
+            results[i] *= postfix;
+            postfix *= nums[i]
+        }
+
+        return results;
+
+    }
+}
